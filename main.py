9a89d04cce20c824d1cae033a6132efdc6efe2d4
timbdn01 <timbdn01@gmail.com>
2025-09-26 19:25:31 -0400

    added user prompt and ai

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+import sys
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    contents = " ".join(args)
+    
+    model = "gemini-2.0-flash-001"
+    response = client.models.generate_content(model=model, contents=contents)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
